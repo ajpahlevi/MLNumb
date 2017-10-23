931fe1fa4229d5aa21d200a23dba72dd056e5916
ajpahlevi <achmad.j.pahlevi@gdplabs.id>
2017-10-23 19:55:39 +0700

    Initial xcode project

---
+++ MLNumb/MLNumb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLNumb
+//
+//  Created by Achmad Jeihan Pahlevi on 10/23/17.
+//  Copyright © 2017 Achmad Jeihan Pahlevi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
